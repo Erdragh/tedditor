159e1d89611ea19bfbdae8919aa368ed9b473317
Erdragh <enderjanyt@gmail.com>
2021-06-09 09:10:32 +0200

    first step into gtk grid

--- README.md
+++ README.md
@@ -4,10 +4,10 @@ A gtk based simple text editor I'm writing for a school project.
 
 ## Can I use it?
 No,
-The current version is just one of the gtk examples, with the first button, but I put my own text into it and updated the application id to `com.github.erdragh.tedditor`, to better reflect that this will be my application.
+The current version is just an example from https://developer.gnome.org/gtk4/4.0, which introduces the gtk grid, but I put my own text into it and updated the application id to `com.github.erdragh.tedditor`, to better reflect that this will be my application.
 
 ## Future plans
-The "Open File" button will not be the intended way to open files, rather I will use a menubar with a `File` menu and the usual `Open...`, `Save` and `Save as...`. I also want to implement opening text files from any file explorer or the command line, such as:
+The buttons will not be the intended way open or save files and also not how to close the application, rather I will use a menubar with a `File` menu and the usual `Open...`, `Save` and `Save as...` and closing will just be done by the default gtk window decorations. I also want to implement opening text files from any file explorer or the command line, such as:
 
 ```bash
 [user@linux ~]$ tedditor ./example.txt
@@ -27,7 +27,6 @@ or
 
 ## Current state
 
-Here is a screenshot of the current state of the application (With the `Fluent-dark-compact` and the `vimix-dark-laptop-ruby` gtk themes):
+Here is a screenshot of the current state of the application (With the `vimix-dark-laptop-ruby` gtk theme):
 
-![fluent](https://github.com/Erdragh/tedditor/raw/main/tedditor.png "Screenshot with `Fluent-dark-compact`")
-![vimix](https://github.com/Erdragh/tedditor/raw/main/tedditor2.png "Screenshot with `vimix-dark-laptop-ruby`")
+![vimix](https://github.com/Erdragh/tedditor/raw/main/tedditor.png "Screenshot with `vimix-dark-laptop-ruby`")
--- tedditor.c
+++ tedditor.c
@@ -1,6 +1,16 @@
 #include <gtk/gtk.h>
 
-static void print_hello (GtkWidget *widget, gpointer data)
+static void open (GtkWidget *widget, gpointer data)
+{
+  g_print ("Warning: Not yet implemented\n");
+}
+
+static void save (GtkWidget *widget, gpointer data)
+{
+  g_print ("Warning: Not yet implemented\n");
+}
+
+static void save_as (GtkWidget *widget, gpointer data)
 {
   g_print ("Warning: Not yet implemented\n");
 }
@@ -9,19 +19,33 @@ static void activate (GtkApplication* app, gpointer user_data)
 {
   GtkWidget *window;
   GtkWidget *button;
-  GtkWidget *box;
+  GtkWidget *grid;
 
+  //Create new window and set its title
   window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "tedditor");
-  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+  gtk_window_set_title (GTK_WINDOW (window), "Tedditor");
 
-  box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
-  gtk_window_set_child (GTK_WINDOW (window), box);
+  //construct the grid / the container for the buttons
+  grid = gtk_grid_new();
 
-  button = gtk_button_new_with_label ("Open File");
-  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+  //Packing the grid / the container into the window
+  gtk_window_set_child (GTK_WINDOW (window), grid);
+
+  button = gtk_button_new_with_label ("Open...");
+  g_signal_connect (button, "clicked", G_CALLBACK (open), NULL);
+
+  //Place the first button in the grid cell (0,0) and make it fill just 1 cell horizontally and vertically (ie no spanning)
+  gtk_grid_attach (GTK_GRID (grid), button, 0, 0, 1, 1);
+
+  button = gtk_button_new_with_label ("Save");
+  g_signal_connect (button, "clicked", G_CALLBACK (save), NULL);
+
+  gtk_grid_attach (GTK_GRID (grid), button, 1, 0, 1, 1);
+
+  button = gtk_button_new_with_label ("Quit");
   g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_window_destroy), window);
-  gtk_box_append (GTK_BOX (box), button);
+
+  gtk_grid_attach (GTK_GRID (grid), button, 0, 1, 2, 1);
 
   gtk_widget_show (window);
 }
